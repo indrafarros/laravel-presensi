9fbff19dd41f16f47c607b04a86e33e6795c3423
indrafarros <indrafarros.fi@gmail.com>
2021-02-28 12:59:39 +0700

    menambah login dan route

--- resources/views/Template/Sidebar.blade.php
+++ resources/views/Template/Sidebar.blade.php
@@ -80,6 +80,14 @@
                         </li>
                     </ul>
                 </li>
+                <li class="nav-item">
+                    <a href="{{ route('logout') }}" class="nav-link">
+                        <i class="nav-icon fas fa-sign-out-alt"></i>
+                        <p>
+                            Sign Out
+                        </p>
+                    </a>
+                </li>
 
             </ul>
         </nav>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,4 +19,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', [HomeController::class, 'index']);
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::get('/index', [LoginController::class, 'index'])->name('index');
+
+Route::post('/login', [LoginController::class, 'login'])->name('login_post');
+
+Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
